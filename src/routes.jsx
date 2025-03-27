1da5af14fdc798aa6384f391c645ab0e2478bb9d
ujjwalghising <ujjwalghising567@gmail.com>
2025-03-28 00:13:55 +0530

    fix

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,20 +3,15 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import SearchResults from "./pages/SearchResults";
 import PrivateRoute from "./components/PrivateRoute";
-import VerifyEmail from "./pages/VerifyEmail";
+
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/products" element={<Products />} />
     <Route path="/cart" element={<Cart />} />
-    <Route path="/verify-email" element={<VerifyEmail />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/login" element={<Login />} />
     <Route path="/search" element={<SearchResults />} />
     <Route path="/products/category/:categoryName?" element={<Products />} />
 
